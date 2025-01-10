862a426e9dbdf07c4f593e2cb4c2135a666f52aa
SHADOWArab <87262605+SHADOWArab@users.noreply.github.com>
2025-01-11 01:09:38 +0530

    modified

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const SECRET_KEY = 'masjideaishasct' ;
 const { sign } = jwt;
 // Middleware
