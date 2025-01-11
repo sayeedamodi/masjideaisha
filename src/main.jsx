720305ab8fdaa5f46a1bd1957ef4e1b3e606a8e1
SHADOWArab <87262605+SHADOWArab@users.noreply.github.com>
2025-01-11 20:57:44 +0530

    added backend routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import App from './App.tsx'
 import React from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<App />} />
+        
+        {/* Redirect to login.html */}
+        <Route path="/login" element={<Navigate to="/login.html" replace />} />
+        
+        {/* Redirect to dashboard.html */}
+        <Route path="/dashboard" element={<Navigate to="/dashboard.html" replace />} />
+      </Routes>
+    </Router>
   </StrictMode>
 )
