114ff958dc6cd16612bd1ed75ad575d3669ec9d5
SHADOWArab <87262605+SHADOWArab@users.noreply.github.com>
2025-01-11 22:10:16 +0530

    updated viteconfig to handle /login and /dashboard files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true, // Automatically open the browser after starting the dev server
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        login: 'public/login.html',      // Add login.html as an entry point
+        dashboard: 'public/dashboard.html', // Add dashboard.html as an entry point
+      }
+    }
+  }
+});
